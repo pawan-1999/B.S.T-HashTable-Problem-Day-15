3dd4b383fea0e7e969f2df3c3f531d6517f26179
Pawan gupta <75203671+pawan-1999@users.noreply.github.com>
2021-08-03 21:34:30 +0530

    Added : Finding Frequency of words in phrase

---
+++ Frequency.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_15_HashTable
+{
+    public class Frequency
+    {
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
+        public void Frequency1(MyMapNode<int, string> hash)
+        {
+            for (int key = 0; key < hash.size; key++)
+            {
+                frequency.TryAdd(hash.Get(key).ToLower(), 0);
+                frequency[hash.Get(key).ToLower()]++;
+            }
+            foreach (KeyValuePair<string, int> item in frequency)
+            {
+                Console.WriteLine($"Frequency of word {item.Key} is {item.Value}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
